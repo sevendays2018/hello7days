844d1d488cf935a7ae188bd77272e631a357c793
sevendays2018 <39212577+sevendays2018@users.noreply.github.com>
2018-05-13 14:38:43 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,35 +1,22 @@
 <?php
-/*
- * PROCESSANDO A MENSAGEM 
- * QUE CHEGA DO BOT
- */
 function processMessage($update) {
-    if($update["result"]["action"] == "buscar.nfe"){
-        $output=array();
-    $output["contextOut"] = array( array( "name" => "test-context",
-        "lifespan"=>3 ,
-        "parameters" => array( "context" => "",
-            "date" => date(DATE_ATOM, mktime(0, 0, 0, 7, 1, 2000)))));
-    $output["speech"] = $speech;
-    $output["displayText"] = $speech;
-    $output["source"] = $intentName;
-    echo json_encode($output);
+    if($update["result"]["action"] == "sayHello"){
+        sendMessage(array(
+            "source" => $update["result"]["source"],
+            "speech" => "Hello from webhook",
+            "displayText" => "Hello from webhook",
+            "contextOut" => array()
+        ));
     }
 }
-/*
- * FUNÇÃO PARA ENVIAR A MENSAGEM
- */
+
 function sendMessage($parameters) {
     echo json_encode($parameters);
 }
 
-/*
- * PEGANDO A REQUISIÇÃO
- */
 $update_response = file_get_contents("php://input");
 $update = json_decode($update_response, true);
 if (isset($update["result"]["action"])) {
     processMessage($update);
 }
-
-
+?>
