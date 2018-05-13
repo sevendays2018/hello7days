c618065c7ac22ccc37b4ae4c1eb31c7ac2824a74
sevendays2018 <39212577+sevendays2018@users.noreply.github.com>
2018-05-13 22:40:55 +0700

    Create report_input_data.php

---
+++ example/report_input_data.php
@@ -0,0 +1,35 @@
+<?php
+/* Webhook Script for 7 Days BOT */
+/* Please note that it support only LINE Messaging API for now */
+
+// Recieve INPUT Data //
+function processMessage($update) {
+    if($update["result"]["action"] == "sayHello"){
+        sendMessage(
+            
+           array(
+  "messages" => [ array(
+      "type" => 4,
+      "payload" => array(
+          "line" => array(
+          "type" => "text",
+          "text" => json_encode($update)
+))
+  )]
+)
+            
+        );
+    }
+}
+
+function sendMessage($parameters) {
+    //$finale = json_decode($parameters);
+    echo json_encode($parameters);
+}
+$update_response = file_get_contents("php://input");
+$update = json_decode($update_response, true);
+if (isset($update["result"]["action"])) {
+    processMessage($update);
+}
+/* Developed by 7 Days Team, Trained by Club Everyday's Members */
+?>
