bf434f46562f039c5770066b5c6c8a8e322ccad3
sevendays2018 <39212577+sevendays2018@users.noreply.github.com>
2018-05-13 16:22:30 +0700

    Update iam.php

--- iam.php
+++ iam.php
@@ -1,24 +1,22 @@
 <?php
 sendMessage(
-           {
- "speech": "TESTED",
-  "messages": [
-    {
-      "type": 4,
-      "platform": "line",
-      "payload": {
-        "line": {
-          "type": "image",
-          "originalContentUrl": "https://i.ytimg.com/vi/bjgbFdfty8Q/maxresdefault.jpg",
-    "previewImageUrl": "https://i.ytimg.com/vi/bjgbFdfty8Q/maxresdefault.jpg"
-      }
-    }
-  }
-  ]
-}
+           array(
+ "speech" => "TESTED",
+  "messages" => array(
+    "type" => 4,
+      "platform" => "line",
+      "payload" => array(
+        "line" => array(
+          "type" => "image",
+          "originalContentUrl" => "https://i.ytimg.com/vi/bjgbFdfty8Q/maxresdefault.jpg",
+    "previewImageUrl" => "https://i.ytimg.com/vi/bjgbFdfty8Q/maxresdefault.jpg"
+    )
+  )
+  )
+)
         );
 function sendMessage($parameters) {
     //$finale = json_decode($parameters);
-    echo $parameters;
+    echo json_encode($parameters);
 }
 ?>
