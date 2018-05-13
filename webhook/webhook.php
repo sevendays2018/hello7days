7b840cd26d6f68a72440a6b4307e4219b854b48b
sevendays2018 <39212577+sevendays2018@users.noreply.github.com>
2018-05-13 23:13:41 +0700

    Update webhook.php

--- webhook/webhook.php
+++ webhook/webhook.php
@@ -13,7 +13,7 @@ function processMessage($update) {
       "payload" => array(
           "line" => array(
           "type" => "text",
-          "text" => json_encode($update)
+          "text" => json_encode($update["userId"])
 ))
   )]
 )
