70447a06842dec8bcace6536644e2c6647447efd
sevendays2018 <39212577+sevendays2018@users.noreply.github.com>
2018-05-13 22:03:59 +0700

    Report INPUT Test V1

--- webhook/webhook.php
+++ webhook/webhook.php
@@ -3,7 +3,9 @@
 /* Please note that it support only LINE Messaging API for now */
 
 // Recieve INPUT Data //
-
+$update_response = file_get_contents("php://input");
+$update = json_decode($update_response, true);
+echo json_encode($update);
 
 /* Developed by 7 Days Team, Trained by Club Everyday's Members */
 ?>
