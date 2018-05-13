e7fc32a074f3f5d699148c73ff52875b7befa479
sevendays2018 <39212577+sevendays2018@users.noreply.github.com>
2018-05-13 23:57:09 +0700

    Update webhook.php

--- webhook/webhook.php
+++ webhook/webhook.php
@@ -5,8 +5,8 @@
 // Recieve INPUT Data //
 function processMessage($update) {
     if($update["result"]["action"] == "sayHello"){
-        $userDataGET = $update;
-        $userDataGET = $userDataGET->'userId';
+        $userDataGET = '{"originalRequest":{"source":"line","data":{"data":{"replyToken":"9b6101a93b4b47b7a0c4a029ceea7bcd","source":{"groupId":"C480d21a690d9d7e8f8b7e4432ad4c5f4","type":"group","userId":"U126269d84ad11b29dd3be5979c1be03f"},"type":"message","message":{"text":"\\run","id":"7952268123059","type":"text"},"timestamp":1526225571229},"source":"line"}},"id":"f4f6a4fd-1567-49ab-b588-ed6b4c5c12d4","timestamp":"2018-05-13T15:32:51.416Z","lang":"th","result":{"source":"agent","resolvedQuery":"\\run","speech":"","action":"sayHello","actionIncomplete":false,"parameters":[],"contexts":[],"metadata":{"intentId":"e8c7f73c-d885-411b-aece-2aec5db299cf","webhookUsed":"true","webhookForSlotFillingUsed":"false","intentName":"Webhook Script Test"},"fulfillment":{"speech":"HEROKU is sleeping now, try again later.","messages":[{"type":0,"speech":"HEROKU is sleeping now, try again later."}]},"score":1},"status":{"code":200,"errorType":"success"},"sessionId":"48d5c9bb-bb47-43f9-9332-84615f803c87"}';
+        $userDataGET = $userDataGET->userId;
         
         $url = 'https://api.line.me/v2/bot/profile/'.$userDataGET;
 $headers = array('Authorization: Bearer Lxqz2e+YBidafMeoIgAYTbcFEJtAaf0EJ9E715p1bU4d6UKk5M8dJ9lvkiMdagnmpkdt+uqdgDERWYJzh2XARz1wZStu4jCWotCDdJ4p1/9TORp6trSz7g1jsoNmB1kmGqdmWGLJ012QWauIs4NckQdB04t89/1O/w1cDnyilFU=');
