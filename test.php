d940be7a42f93f57bf1e768e7a77abaa8345c009
sevendays2018 <39212577+sevendays2018@users.noreply.github.com>
2018-05-13 14:43:33 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,9 +1,11 @@
 <?php
+$update_response = file_get_contents("php://input");
+$update = json_decode($update_response, true);
 $iamwho = array(
-            "line": {
-    "type": "text",
-    "text": "tested"
-  }
+            "source" => $update["result"]["source"],
+            "speech" => "Hello from webhook",
+            "displayText" => "Hello from webhook",
+            "contextOut" => array()
         );
  echo json_encode($iamwho);
 /*function processMessage($update) {
