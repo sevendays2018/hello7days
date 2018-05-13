88048831de1b5de41b744e41ad5d03c10538a504
sevendays2018 <39212577+sevendays2018@users.noreply.github.com>
2018-05-13 15:37:32 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,56 +1,22 @@
 <?php
 function processMessage($update) {
-    //if the action is homework then run below
-    if($update["result"]["action"] == "homework"){
-      
-     //trying to create a basic card
-      array_push($messages, array(
-        "buttons"=> [
-          [
-            "title"=> "Button title",
-            "openUrlAction"=> [
-              "url"=> "https://linkUrl.com"
-              ]
-            ]
-          ],
-          "formattedText"=> "AoG Card Description",
-          "image"=>[
-            "url"=>'https://linkUrl.com',
-            "accessibility_text"=>'image-alt'
-          ],
-          "platform"=> "google",
-          "subtitle"=> "card subtitle",
-          "title"=> "Card title",
-          "type"=> "basic_card",
-          )
-         );
-
-         $response=array(
+    if($update["result"]["action"] == "sayHello"){
+        sendMessage(array(
             "source" => $update["result"]["source"],
-            "speech" => "Speech for response",  //This is the only thing that returns
-            "messages" => $messages,  //This is what I want to return, I think
+            "speech" => "Hello from webhook",
+            "displayText" => "Hello from webhook",
             "contextOut" => array()
-          );
-          sendMessage($response);
-      }
-  }
-  /*
-   * Send back message
-  */
-  function sendMessage($messageout) {
-      print_r ($messageout);
-      echo json_encode($messageout);
-  }
+        ));
+    }
+}
 
-  //Get the json message and decode
-  $json = file_get_contents('php://input');
-  $request = json_decode($json, true);
-  $action = $request["result"]["action"];
-  $parameters = $request["result"]["parameters"];
-  $messages=[];
+function sendMessage($parameters) {
+    echo json_encode($parameters);
+}
 
-  //If the message has an action attached, call the processMessage function
-  if (isset($update["result"]["action"])) {
-      processMessage($update);
-  }
+$update_response = file_get_contents("php://input");
+$update = json_decode($update_response, true);
+if (isset($update["result"]["action"])) {
+    processMessage($update);
+}
 ?>
