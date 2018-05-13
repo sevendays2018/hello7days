6a54048e6e972bbd740d883ae35e6cae3f76e44e
sevendays2018 <39212577+sevendays2018@users.noreply.github.com>
2018-05-13 15:55:01 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,19 +1,27 @@
 <?php
 function processMessage($update) {
     if($update["result"]["action"] == "sayHello"){
-        var messageData = {
+        sendMessage(array(
+            "source" => $update["result"]["source"],
+            "speech" => {
   "line": {
     "type": "image",
     "originalContentUrl": "https://i.ytimg.com/vi/bjgbFdfty8Q/maxresdefault.jpg",
     "previewImageUrl": "https://i.ytimg.com/vi/bjgbFdfty8Q/maxresdefault.jpg"
-  }
-}
-        sendMessage($messageData);
+  },
+            "displayText" => {
+  "line": {
+    "type": "image",
+    "originalContentUrl": "https://i.ytimg.com/vi/bjgbFdfty8Q/maxresdefault.jpg",
+    "previewImageUrl": "https://i.ytimg.com/vi/bjgbFdfty8Q/maxresdefault.jpg"
+  },
+            "contextOut" => array()
+        ));
     }
 }
 
 function sendMessage($parameters) {
-    echo $parameters;
+    echo json_encode($parameters);
 }
 
 $update_response = file_get_contents("php://input");
@@ -22,3 +30,4 @@ if (isset($update["result"]["action"])) {
     processMessage($update);
 }
 ?>
+
