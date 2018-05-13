6123293ac93fb8bd21270f8d97bd200357906f52
sevendays2018 <39212577+sevendays2018@users.noreply.github.com>
2018-05-13 21:33:43 +0700

    Update webhook.php

--- webhook/webhook.php
+++ webhook/webhook.php
@@ -1,3 +1,9 @@
 <?php
+/* Webhook Script for 7 Days BOT */
+/* Please note that it support only LINE Messaging API for now */
 
+// Recieve INPUT Data //
+
+
+/* Developed by 7 Days Team, Trained by Club Everyday's Members */
 ?>
