8222dcffe683b2286f93ad779b93004e897ab356
sevendays2018 <39212577+sevendays2018@users.noreply.github.com>
2018-05-13 14:34:25 +0700

    Update test.php

--- test.php
+++ test.php
@@ -3,21 +3,24 @@
  * PROCESSANDO A MENSAGEM 
  * QUE CHEGA DO BOT
  */
-function processMessage(update) {
-    if(update["result"]["text"] == "ทดสอบ"){
-        sendMessage(array(
-            "source" => update["result"]["source"],
-            "speech" => "..........TEXT HERE...........",
-            "displayText" => ".........TEXT HERE...........",
-            "contextOut" => array()
-        ));
+function processMessage($update) {
+    if($update["result"]["action"] == "buscar.nfe"){
+        $output=array();
+    $output["contextOut"] = array( array( "name" => "test-context",
+        "lifespan"=>3 ,
+        "parameters" => array( "context" => "",
+            "date" => date(DATE_ATOM, mktime(0, 0, 0, 7, 1, 2000)))));
+    $output["speech"] = $speech;
+    $output["displayText"] = $speech;
+    $output["source"] = $intentName;
+    echo json_encode($output);
     }
 }
 /*
  * FUNÇÃO PARA ENVIAR A MENSAGEM
  */
-function sendMessage(parameters) {
-    echo json_encode(parameters);
+function sendMessage($parameters) {
+    echo json_encode($parameters);
 }
 
 /*
@@ -28,4 +31,5 @@ $update = json_decode($update_response, true);
 if (isset($update["result"]["action"])) {
     processMessage($update);
 }
-?>
+
+
